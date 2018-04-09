2b644caa8d0da8e60bc43f3fa0754614cd415a6d
Jay <jshridha@gmail.com>
2018-04-09 10:31:42 -0400

    Set permissions to root on startup; Don't redownload blueiris if already downloaded.

--- blueiris.sh
+++ blueiris.sh
@@ -2,17 +2,20 @@
 
 BLUEIRIS_EXE="/root/prefix32/drive_c/Program Files/Blue Iris 4/BlueIris.exe"
 PREFIX_DIR="/root/prefix32"
+INSTALL_EXE="/root/blueiris.exe"
 
 if [ ! -d "$PREFIX_DIR/drive_c" ]; then
     mv /root/prefix32_original/* /root/prefix32
 fi
 
+chown -R root:root /root/prefix32
 
 if [ ! -e "$BLUEIRIS_EXE" ] ; then
-    wget http://blueirissoftware.com/blueiris.exe
+    if [ ! -e "$INSTALL_EXE"] ; then
+        wget http://blueirissoftware.com/blueiris.exe
+    fi
     wine blueiris.exe /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-
     rm blueiris.exe
 fi
 
 wine "$BLUEIRIS_EXE"
-
